1366776f462326ebdc832c9d4138ddef9d22dd17
Aiden Arnkels-Webb <aiden@aidenwebb.com>
2023-05-03 20:25:45 +0100

    Add gnome extensions - vitals

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -6,7 +6,13 @@ python3 get-pip.py --user
 
 # Install Ansible
 python3 -m pip install --user ansible
+
+# Install ansible collections
+ansible-galaxy collection install community.general
+
+# Install ansible roles
 ansible-galaxy install gantsign.oh-my-zsh
+ansible-galaxy install petermosmans.customize-gnome
 
 # Install gnumake
 
